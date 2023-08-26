863fac09f72d66b2b28d29d4356ee2b89806025e
JChavannes01 <janko.chavannes@gmail.com>
2023-08-26 12:33:40 +0200

    exercise 3.15 - 3.18

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const Person = require("./models/person");
 
 const app = express();
+
+// ==============================
+//  MIDDLEWARE
+// ==============================
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
@@ -14,46 +18,24 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
-const generateId = () => {
-  let id;
-  do {
-    id = Math.floor(Math.random() * 1e6);
-  } while (persons.findIndex((p) => p.id === id) >= 0);
-  return id;
-};
+// ==============================
+// ENDPOINTS
+// ==============================
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello world!</h1>");
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`
-  );
+  Person.find({})
+    .then((persons) => {
+      const lines = [
+        `<p>Phonebook has info for ${persons.length} people</p>`,
+        `<p>${new Date()}</p>`,
+      ];
+      response.send(lines.join("\n"));
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons", (request, response) => {
@@ -63,22 +45,26 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  // const id = Number(request.params.id);
-  // const person = persons.find((person) => person.id === id);
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
-  // if (person) {
-  //   response.json(person);
-  // } else {
-  //   response.status(404).end();
-  // }
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error))
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((note) => note.id !== id);
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -91,29 +77,67 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({ error: "number missing" });
   }
 
-  // if (persons.findIndex((p) => p.name === body.name) >= 0) {
-  //   return response.status(400).json({ error: "name must be unique" });
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
-    // id: generateId(),
   });
 
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
-
-  // console.log(person);
-  // persons = persons.concat(person);
-  // response.json(person);
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  }
+  if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  // option new:true causes the updatedPerson object to contain modified values instead of old values.
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+// ==============================
+// Error handlers
+// ==============================
+
+// This should be the final "normal" (i.e. not error handler) route/middleware added to express
 const unknownEndpointHandler = (request, response, next) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpointHandler);
+
+// First error handler (extra param for error). Should be added AFTER all middlewares/routes
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
+
+// ==============================
+// Application start
+// ==============================
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
